9223e4b36c7054b4acf221e2f3222f43310abcf0
anya169 <oxina.anya@yandex.ru>
2026-04-10 16:41:06 +0700

    Импортируем наши функции в компонент калькулятора
    На основной странице мы работаем с компонентом калькулятора, поэтому все функции будем пробрасывать через него в другие компоненты. В панели кнопок мы обозначали 3 функции. Передадим их в нашу панель, считая значение через функции из логики калькулятора. В значение дисплея будем передавать наше выражение, которое мы устанавливаем через функции.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import '../styles/Calculator.css';
+import { appendSymbol, calculate, clearAll } from '../scripts/calculatorLogic';
 
 
 function Calculator() {
+    // Текущее выражение на дисплее, используем функцию setExpression, чтобы устанавливать его
+    // Так как в React запрещено менять напрямую через =
+    // По умолчанию равно нулю
+    const [expression, setExpression] = useState('0');
+
+    // Функция обработки нажатия на цифры и операторы
+    const handleButtonClick = (symbol) => {
+        // Заменяем предыдущее выражение (prev от previous - предыдущий)
+        // На новое, которое нам вернула функция из нашей логики калькулятора
+        setExpression(prev => appendSymbol(prev, symbol));
+    };
+
+    // Функция подсчета
+    const handleCalculate = () => {
+        setExpression(prev => calculate(prev));
+    };
+
+    // Функция очистки
+    const handleClear = () => {
+        setExpression(clearAll());
+    };
+
     return (
         <div className="calculator-container">
-            <Display value="0" />
-            <ButtonPanel />
+            <Display value={expression} />
+            <ButtonPanel 
+                onButtonClick={handleButtonClick}
+                onCalculate={handleCalculate}
+                onClear={handleClear}
+            />
         </div>
     );
 }
